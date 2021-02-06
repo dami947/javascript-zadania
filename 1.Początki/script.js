223130e65bdd211e9272f44bfc9075755bc87d16
dami947 <78652039+dami947@users.noreply.github.com>
2021-02-06 17:05:33 +0100

    Update script.js

---
+++ 1.Początki/script.js
@@ -0,0 +1,14 @@
+//Zadanie 1
+
+console.log("Twoje Imię");  //w konsoli wyświetlił się ten napis
+
+//Zadanie 2
+12 + 32 = 44
+52/16 = 3.25
+5-2 = 10
+
+//Zadanie 3
+
+"Hello world" //nic
+Hello world //Błąd - nieokreślona wartość/typ
+"Hello world //Błąd - brak drugiego nawiasu
